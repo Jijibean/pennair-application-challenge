83af135ddd09c3ef83561260b880e6f4bd2967c6
Evan Ji <scpevan06@gmail,com>
2026-09-07 21:46:17 -0400

    Created Clean Mask for all the Images

---
+++ src/part1Image.py
@@ -0,0 +1,19 @@
+import cv2
+
+img = cv2.imread('data/PennAirImageStatic.png')
+if img is None:
+    raise FileNotFoundError("check the path")
+
+blurImg = cv2.GaussianBlur(img, (5, 5), 0)
+hsv = cv2.cvtColor(blurImg, cv2.COLOR_BGR2HSV)
+h, s, v = cv2.split(hsv)
+
+mask = cv2.inRange(v, 130, 255)
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+
+cv2.imshow('mask', mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
